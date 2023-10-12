c07c18ef1b887b65d2c0bf7dc42a9fb9b7d2f84d
Abe Levitan <abraham.levitan@psi.ch>
2023-10-12 10:42:37 +0200

    Add the collate step, update the realization step, and get the full slurm scheduling command working

--- slurm/collate_sunflower_design.sh
+++ slurm/collate_sunflower_design.sh
@@ -2,10 +2,10 @@
 #SBATCH -p day
 #SBATCH -A gpu
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 6
+#SBATCH --cpus-per-task 1
 
 module load anaconda/2023-06-19
 source /opt/psi/TOMCAT/anaconda/2023-06-19/conda/etc/profile.d/conda.sh
 conda activate zpom
 
-realize-sunflower-design $1 $2 $3 -n $SLURM_ARRAY_TASK_ID --chunk_size 4096 -np $4
+collate-sunflower-design $1 
--- slurm/schedule_sunflower_rzp.py
+++ slurm/schedule_sunflower_rzp.py
@@ -11,7 +11,10 @@ parser = argparse.ArgumentParser(
 # slurm jobs to fail.
 parser.add_argument('plan_file', type=str, help='The plan file to use.')
 parser.add_argument('n_zps', type=int, help='The total number of zps in the plan file')
-parser.add_argument('--gpu', action='store_true', help='Whether to use the GPUs')
+parser.add_argument('grating_level', type=float, help='The fraction of the max amplitude to saturate at. Setting this to 1 provides best quality, at the expense of efficiency. 0.6 is usually a reasonable middle ground, and 0 produces a zone plate without any amplitude variation.')
+parser.add_argument('buttress_width', type=float, help='The width of the buttresses, in nm. 0 will produce no buttresses')
+parser.add_argument('--gpu', action='store_true', help='Whether to use the GPUs for the design phase.')
+parser.add_argument('--n_processes', '-np', type=int, default=6, help='The number of processes to allocated to the realization stage.')
 parser.add_argument('--design-time','-dt', type=str, default=None, help='Time limit per design step. Default is default for the day queue on cpu or default for the gpu-week queue on gpu.')
 parser.add_argument('--realize-time','-rt', type=str, default=None, help='Time limit per design step. Default is default for the day queue.')
 
@@ -28,19 +31,52 @@ else:
 
 print('Scheduling the design steps:')
 
-design_job = ['sbatch',
-              jobarray,
-              design_command,
-              args.plan_file]
+design_job = [
+    'sbatch',
+    jobarray,
+    design_command,
+    args.plan_file
+]
 
 print(design_job)
-#exit()
-slurm_output = subprocess.check_output(design_job)
-
-print(slurm_output)
-exit()
+slurm_output = subprocess.check_output(design_job).decode()
 
 job_id = int(slurm_output.split(' ')[-1])
 
-print('Design steps have Job ID', job_id,'\n')
+print('\nDesign steps have Job ID', job_id,'\n')
 print('Scheduling the realization steps')
+
+design_folder = args.plan_file[:-8:] + '_intermediate_design'
+
+realize_job = [
+    'sbatch',
+    jobarray,
+    '--cpus-per-task=' + str(args.n_processes),
+    '--dependency=afterok:'+str(job_id),
+    'realize_sunflower_design.sh',
+    design_folder,
+    str(args.grating_level),
+    str(args.buttress_width),
+    str(args.n_processes),    
+]
+
+slurm_output = subprocess.check_output(realize_job).decode()
+job_id = int(slurm_output.split(' ')[-1])
+
+print('Realization steps have Job ID', job_id, '\n')
+
+realization_folder = args.plan_file[:-8:] \
+                     + ('_GL=%0.2f_BW=%0.2fnm'
+                        % (args.grating_level, args.buttress_width))
+
+collate_job = [
+    'sbatch',
+    '--dependency=afterok:'+str(job_id),
+    'collate_sunflower_design.sh',
+    realization_folder,
+]
+
+slurm_output = subprocess.check_output(collate_job).decode()
+job_id = int(slurm_output.split(' ')[-1])
+
+print('Collation step has Job ID', job_id, '\n')
